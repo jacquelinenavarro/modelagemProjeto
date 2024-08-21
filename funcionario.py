4a606e506ecaddb04454acb056e79455a5592f4f
Jacqueline Navarro <154712329+jacquelinenavarro@users.noreply.github.com>
2024-08-21 15:23:20 -0300

    Delete funcionario.py

--- funcionario.py
+++
@@ -1,9 +0,0 @@
-class Funcionario:
-    def __init__(self, id, nome, cargo, genero):
-        self.id = id
-        self.nome = nome
-        self.cargo = cargo
-        self.genero = genero
-
-    def descrever_funcionario(self):
-        return f"Funcionario(id={self.id}, nome={self.nome}, cargo={self.cargo}, genero={self.genero})"
